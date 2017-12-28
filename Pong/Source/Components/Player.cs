bb1006e8701e521fd83014664960c4967a299918
Liquidz <pascal.hobza@outlook.com>
2017-12-28 14:05:21 +0100

    Fixed bug, crash if no arduino is connected

--- Pong/Source/Components/Player.cs
+++ Pong/Source/Components/Player.cs
@@ -4,76 +4,80 @@ using System.Windows.Forms;
 
 namespace Pong.Source.Components
 {
-	/// <summary>
-	/// Description of Player.
-	/// </summary>
-	public class Player
-	{
+    /// <summary> This Class holds the variables for one player. </summary>
+    public class Player
+    {
+        /// <summary> The movement speed of the player. </summary>
 		private const int Speed = 20;
 
+        /// <summary> The three sizes of the panel configured over the options. </summary>
 		public const int PanelSmall = 50;
-		public const int PanelNormal = 150;
-		public const int PanelBig = 200;
-        
-		private readonly Panel _pnl;
-		private readonly Label _lblScore;
+        public const int PanelNormal = 150;
+        public const int PanelBig = 200;
 
-		public bool PlayerUp;
-		public bool PlayerDown;
+        private readonly Panel _pnl;
+        private readonly Label _lblScore;
 
-		private int _score;
+        public bool PlayerUp;
+        public bool PlayerDown;
 
-		public Player(Panel pnl, Label lblScore)
-		{
-			_pnl = pnl;
-			_lblScore = lblScore;
-		}
+        private int _score;
 
-		public void Move()
-		{
-			if (PlayerUp)
-			{
-				_pnl.Location = new Point(_pnl.Location.X, Math.Max(World.Upper, _pnl.Location.Y - Speed));
-			}
-			else if (PlayerDown)
-			{
-				_pnl.Location = new Point(_pnl.Location.X, Math.Min(World.Bottom - _pnl.Size.Height, _pnl.Location.Y + Speed));
-			} 
-		}
+        public Player(Panel pnl, Label lblScore)
+        {
+            _pnl = pnl;
+            _lblScore = lblScore;
+        }
 
-		internal void Score()
-		{
-			_score++;
-			_lblScore.Text = _score.ToString();
-			Pong.Instance.MainForm.ResetRound();
-			Pong.DebugMessage("Ein Punkt wurde vergeben.");
-		}
+        /// <summary> Let the player move, depending which button is pressed, when no Arduino is connected. </summary>
+        public void Move()
+        {
+            if (PlayerUp)
+            {
+                _pnl.Location = new Point(_pnl.Location.X, Math.Max(World.Upper, _pnl.Location.Y - Speed));
+            }
+            else if (PlayerDown)
+            {
+                _pnl.Location = new Point(_pnl.Location.X, Math.Min(World.Bottom - _pnl.Size.Height, _pnl.Location.Y + Speed));
+            }
+        }
 
-		internal void SetRelativePanelPosition(int percentage)
-		{
-			double position = (World.Bottom - _pnl.Size.Height - World.Upper) / 100f * percentage;
-			_pnl.Location = new Point(_pnl.Location.X, Math.Min(World.Bottom - _pnl.Size.Height, Convert.ToInt32(position) + World.Upper));
-		}
-        
-		public void SetPanelHeight(int height)
-		{
-			_pnl.Height = height;
-			Pong.DebugMessage("Höhe des Balken wurde auf " + height + " gesetzt!");
-		}
+        /// <summary> Gives this player a scorepoint. </summary>
+        internal void Score()
+        {
+            _score++;
+            _lblScore.Text = _score.ToString();
+            Pong.Instance.MainForm.ResetRound();
+            Pong.DebugMessage("Ein Punkt wurde vergeben.");
+        }
 
-		internal bool Hits(PictureBox ball)
-		{
-			return _pnl.Bounds.IntersectsWith(ball.Bounds);
-		}
+        /// <summary> Sets the players panel position relative to the poti value. </summary>
+        internal void SetRelativePanelPosition(int percentage)
+        {
+            double position = (World.Bottom - _pnl.Size.Height - World.Upper) / 100f * percentage;
+            _pnl.Location = new Point(_pnl.Location.X, Math.Min(World.Bottom - _pnl.Size.Height, Convert.ToInt32(position) + World.Upper));
+        }
 
-		internal void Reset()
-		{
-			_pnl.Location = new Point(_pnl.Location.X, ((World.Bottom - World.Upper) / 2) - (_pnl.Size.Height / 2));
-		}
-        
-		public void Resize()
-		{
-			_pnl.Location = new Point(_pnl.Location.X, Math.Min(World.Bottom - _pnl.Size.Height, _pnl.Location.Y));
-		}
-	}
+        /// <summary> Changes the panel height. </summary>
+        public void SetPanelHeight(int height)
+        {
+            _pnl.Height = height;
+            Pong.DebugMessage("Höhe des Balken wurde auf " + height + " gesetzt!");
+        }
+
+        internal bool Hits(PictureBox ball)
+        {
+            return _pnl.Bounds.IntersectsWith(ball.Bounds);
+        }
+
+        internal void Reset()
+        {
+            _pnl.Location = new Point(_pnl.Location.X, ((World.Bottom - World.Upper) / 2) - (_pnl.Size.Height / 2));
+        }
+
+        public void Resize()
+        {
+            _pnl.Location = new Point(_pnl.Location.X, Math.Min(World.Bottom - _pnl.Size.Height, _pnl.Location.Y));
+        }
+    }
 }
