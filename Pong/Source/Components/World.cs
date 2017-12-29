3eba245008dd8f4b9bf8d7ec541913c3d174ceb0
Liquidz <pascal.hobza@outlook.com>
2017-12-29 18:00:02 +0100

    Removed ToolStripMenu and added ContextStripMenu

--- Pong/Source/Components/World.cs
+++ Pong/Source/Components/World.cs
@@ -3,7 +3,7 @@
     /// <summary> Holds the bounds for the gameworld. </summary>
     public static class World
     {
-        public static int Upper = 24, Bottom, Left = 0, Right;
+        public static int Upper = 0, Bottom, Left = 0, Right;
 
         public static void SetBounds(int height, int width)
         {
