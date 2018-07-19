067e494414dcac6696e43203b69fc47c46771eb6
Liquidz <pascal.hobza@outlook.com>
2018-07-19 14:18:37 +0200

    Template Class for the new Game Bounce

---
+++ Pong/Bounce/Bounce.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroTable.Bounce
+{
+    public class Bounce
+    {
+        public Bounce()
+        {
+
+        }
+
+        internal void Show()
+        {
+
+        }
+
+        internal void Hide()
+        {
+
+        }
+
+        internal void Start()
+        {
+
+        }
+
+        internal void Reset()
+        {
+
+        }
+    }
+}
