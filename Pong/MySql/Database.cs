2e3941a3d1429f823cce945a8696cea05e6c227c
LiquidzDEV <liquidzde@gmail.com>
2018-10-09 14:32:00 +0200

    Changed Database Info

--- Pong/MySql/Database.cs
+++ Pong/MySql/Database.cs
@@ -21,10 +21,10 @@ namespace RetroTable.MySql
             connection = new MySqlConnectionStringBuilder();
 
             connection.Server = "85.214.66.124";
-            connection.UserID = "pong";
+            connection.UserID = "retro";
             connection.Password = "BQnmDXC74p_WRT$8";
             connection.Port = 3306;
-            connection.Database = "pong";
+            connection.Database = "retro";
 
             User = connection.Connection().AsParallel<TableUser>();
             Records = connection.Connection().AsParallel<TableRecords>();
