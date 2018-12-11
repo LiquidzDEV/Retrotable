f41ded5c9a976b811dad8f4160efa7e90c3b7bf5
Liquidz <pascal.hobza@outlook.com>
2018-12-11 14:36:23 +0100

    Added Test to watch the Datatransfer

---
+++ Pong/Test/ArduinoDataTest.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace RetroTable.Test
+{
+    partial class ArduinoDataTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvwData = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvwData
+            // 
+            this.lvwData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvwData.Location = new System.Drawing.Point(12, 201);
+            this.lvwData.Name = "lvwData";
+            this.lvwData.Size = new System.Drawing.Size(283, 216);
+            this.lvwData.TabIndex = 0;
+            this.lvwData.UseCompatibleStateImageBehavior = false;
+            this.lvwData.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Pin";
+            this.columnHeader1.Width = 52;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Value";
+            this.columnHeader2.Width = 83;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Time";
+            this.columnHeader3.Width = 144;
+            // 
+            // ArduinoDataTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 429);
+            this.Controls.Add(this.lvwData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ArduinoDataTest";
+            this.Text = "ArduinoDataTest";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvwData;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
---
+++ Pong/Test/ArduinoDataTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RetroTable.Test
+{
+    public partial class ArduinoDataTest : Form
+    {
+        private static ArduinoDataTest Instance;
+
+        public ArduinoDataTest()
+        {
+            InitializeComponent();
+            Instance = this;
+        }
+
+        public static void AddData(int pin, int value)
+        {
+            if (Instance == null) return;
+
+            var item = new ListViewItem(pin.ToString());
+            item.SubItems.Add(value.ToString());
+            item.SubItems.Add(DateTime.Now.ToShortTimeString());
+            Instance.lvwData.Items.Add(item);
+
+            if(Instance.lvwData.Items.Count > 100)
+            {
+                Instance.lvwData.Items.RemoveAt(0);
+            }
+        }
+    }
+}
