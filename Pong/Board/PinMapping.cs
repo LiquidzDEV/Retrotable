8ceac733851899fcf1fe7e293b72034a9f6c1bc9
Liquidz <pascal.hobza@outlook.com>
2018-12-24 02:58:32 +0100

    Some cleanup

--- Pong/Board/PinMapping.cs
+++ Pong/Board/PinMapping.cs
@@ -8,18 +8,18 @@
     {
         Player1Buttons = 7,
         Player2Buttons = 8,
-        EncoderSW = 9,//1
-        EncoderDT = 2,
-        EncoderCLK = 3,
-        Player1SliderSensLeft = 4,
-        Player1SliderSensRight = 5,
+        EncoderSW = 0,
+        EncoderDT = 15,
+        EncoderCLK = 14,
+        Player1SliderSensLeft = 9,
+        Player1SliderSensRight = 10,
         Player1SliderTotal = 100,
-        Player2SliderSensLeft = 11,//14
-        Player2SliderSensRight = 12,//15
+        Player2SliderSensLeft = 13,
+        Player2SliderSensRight = 2,
         Player2SliderTotal = 101,
-        Player1ButtonLeftLed = 16,//9
-        Player1ButtonRightLed = 10,
-        Player2ButtonLeftLed = 14,//11
-        Player2ButtonRightLed = 15,//12
+        Player1ButtonLeftLed = 4,
+        Player1ButtonRightLed = 5,
+        Player2ButtonLeftLed = 6,
+        Player2ButtonRightLed = 12,
     }
 }
